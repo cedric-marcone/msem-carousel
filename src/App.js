7ab195a94ba50ee4a2e868dcc59abc5b70e25bef
Cédric Marcone <cedric.marcone@valraiso.fr>
2020-03-26 09:49:17 +0100

    initial revision

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Carousel from "./Carousel";
+import "./App.css";
 
-function App() {
+const host = "https://phototheque.mon-sejour-en-montagne.com";
+const prefix = `${host}/images/msem/1920_946/`;
+const slides = [
+  { text: "Photo 1", img: "home-bg.jpg" },
+  { text: "Photo 2", img: "home-bg-accommodation-1.jpg" },
+  { text: "Photo 3", img: "home-bg-skipasses-1.jpg" }
+];
+const App = () => {
+  const [current, setCurrent] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="carousel-outer">
+        <Carousel
+          auto
+          current={current}
+          prevButton={
+            <button className="carousel__button carousel__button--left">
+              <svg className="carousel__icon" viewBox="0 0 24 24">
+                <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
+              </svg>
+            </button>
+          }
+          nextButton={
+            <button className="carousel__button carousel__button--right">
+              <svg className="carousel__icon" viewBox="0 0 24 24">
+                <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
+              </svg>
+            </button>
+          }
         >
-          Learn React
-        </a>
-      </header>
+          {slides.map((slide, index) => (
+            <div className="slide" key={index}>
+              <div className="text">{slide.text}</div>
+              <img src={`${prefix}${slide.img}`} alt="" />
+            </div>
+          ))}
+        </Carousel>
+      </div>
+      <div>
+        {slides.map((_, index) => (
+          <button key={index} onClick={() => setCurrent(index)}>
+            {index}
+          </button>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Carousel.js
@@ -0,0 +1,140 @@
+import React, { useRef, useEffect, memo, cloneElement } from "react";
+
+const raf = requestAnimationFrame;
+const doubleRaf = cb => raf(() => raf(cb));
+
+const reorderChildren = (children, initial) => {
+  return [
+    ...children.slice(initial, children.length),
+    ...children.slice(0, initial)
+  ];
+};
+
+const patchProps = ({ type, props }) => {
+  if (type !== "img") return props;
+  const { src, ...rest } = props;
+  return { ...rest, "data-src": src };
+};
+
+const patchChildren = children => {
+  return children.map(child => ({
+    ...child,
+    props: {
+      ...patchProps(child),
+      children: Array.isArray(child.props.children)
+        ? patchChildren(child.props.children)
+        : child.props.children
+    }
+  }));
+};
+
+const loadImage = img => {
+  return new Promise(resolve => {
+    if (!img.src) {
+      img.onload = resolve;
+      img.src = img.dataset.src;
+      delete img.dataset.src;
+    } else {
+      resolve();
+    }
+  });
+};
+
+const loadImages = slide => {
+  const imgs = Array.from(slide.querySelectorAll("img"));
+  return Promise.all(imgs.map(loadImage));
+};
+
+const styles = {
+  position: "relative",
+  width: "100%",
+  overflow: "hidden"
+};
+const innerStyles = {
+  display: "flex",
+  transition: "transform cubic-bezier(.4, 0, .2, 1)"
+};
+
+const Carousel = ({
+  duration = 3000,
+  transition = 240,
+  auto,
+  prevButton,
+  nextButton,
+  children,
+  current = 2
+}) => {
+  const inner = useRef();
+  const interval = useRef();
+  const transitioning = useRef();
+  const transitionMs = `${transition}ms`;
+
+  const beginTransition = () => {
+    if (transitioning.current) return false;
+    transitioning.current = true;
+    clearInterval(interval.current);
+    return true;
+  };
+
+  const endTransition = () => {
+    transitioning.current = false;
+    if (auto) interval.current = setInterval(next, duration);
+  };
+
+  const prev = () => {
+    if (!beginTransition()) return;
+    const el = inner.current;
+    const first = el.firstChild;
+    const last = el.lastChild;
+    loadImages(last).then(() => {
+      el.style.transitionDuration = "0s";
+      el.style.transform = `translate(-100%, 0)`;
+      el.removeChild(last);
+      el.insertBefore(last, first);
+      doubleRaf(() => {
+        el.style.transitionDuration = transitionMs;
+        el.style.transform = `translate(0, 0)`;
+        setTimeout(endTransition, transition);
+      });
+    });
+  };
+
+  const next = () => {
+    if (!beginTransition()) return;
+    const el = inner.current;
+    const first = el.firstChild;
+
+    loadImages(first.nextSibling).then(() => {
+      el.style.transitionDuration = transitionMs;
+      el.style.transform = `translate(-100%, 0)`;
+      setTimeout(() => {
+        el.style.transitionDuration = "0s";
+        el.style.transform = `translate(0, 0)`;
+        el.removeChild(first);
+        el.appendChild(first);
+        doubleRaf(() => {
+          el.style.transitionDuration = transitionMs;
+          endTransition();
+        });
+      }, transition);
+    });
+  };
+
+  useEffect(() => {
+    const first = inner.current.firstChild;
+    loadImages(first).then(endTransition);
+    return beginTransition;
+  });
+
+  return (
+    <div style={styles}>
+      <div style={innerStyles} ref={inner}>
+        {patchChildren(reorderChildren(children, current))}
+      </div>
+      {prevButton && cloneElement(prevButton, { onClick: prev })}
+      {nextButton && cloneElement(nextButton, { onClick: next })}
+    </div>
+  );
+};
+
+export default memo(Carousel);
